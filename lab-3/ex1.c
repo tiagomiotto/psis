75d93ae50ba352e954bc460eaf0e913fe3a92175
tiagomiotto <ttiago.miotto@hotmail.com>
2018-03-12 23:38:21 +0000

    Adding lab3

---
+++ lab-3/ex1.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(){
+pid_t pids[10];
+int i;
+int n=10;
+int sec;
+for (i = 0; i < n; ++i)
+{
+	if((pids[i] = fork())<0){
+		perror("fork");
+		abort();
+	}
+	else if (pids[i] ==0){
+		srand(getpid());
+		sec=rand()%11;
+		sleep(sec);
+		printf("I am proccess %d and i sleept for %d \n", getpid(), sec);
+		exit(0);
+	}
+}
+
+int status;
+pid_t pid;
+while(n>0){
+	pid = wait(&status);
+	--n;
+}
+return(0);
+}
---
+++ lab-3/ex2.c
@@ -0,0 +1,58 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(){
+pid_t pids[10];
+int i;
+int n=10;
+int sec;
+for (i = 0; i < n; ++i)
+{
+	if((pids[i] = fork())<0){
+		perror("fork");
+		abort();
+	}
+	else if (pids[i] ==0){
+		srand(getpid());
+		sec=rand()%11;
+		sleep(sec);
+		
+		exit(sec);
+	}
+}
+
+int status;
+pid_t pid,pid2;
+
+while(n>0){
+	pid = wait(&status);
+	if (WIFEXITED(status)){
+    	int returned = WEXITSTATUS(status);
+		printf("Process %d sleept for %d seconds and exited\n", pid, returned);
+	}
+	
+	--n;
+
+	if(n<10){
+
+	if((pid2 = fork())<0){
+		perror("fork");
+		abort();
+	}
+	else if (pid2 ==0){
+		srand(getpid());
+		sec=rand()%11;
+		sleep(sec);
+		
+		exit(sec);
+	}
+	n++;
+	printf("Processes running %d\n",n );	
+	}
+}
+return(0);
+}
