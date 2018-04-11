201822d9a93a1e583098432c4b7e0f7b68b53ed6
public scdeec <public@blackrussian.scdeec.tecnico.ulisboa.pt>
2018-04-11 14:28:33 +0100

    Functional lab6 with Unix Sockets

--- lab6_v2/clipboard.c
+++ lab6_v2/clipboard.c
@@ -7,23 +7,26 @@ int main(){
 	socklen_t addr_size;
 	struct addrinfo hints, *res,*p;
 	int my_fd, new_fd,aux2;
-	char client[INET6_ADDRSTRLEN];
+	int client;
 	char data[10][10];
 	Mensagem aux;
 	char *msg = malloc(sizeof(Mensagem));
 	int len_data;
 	char buff[10];
 
+	//Using Unix sockets
 	if((my_fd=socket(AF_UNIX,SOCK_STREAM,0))<0){
 		printf("Socket unsuccessful\n");
 		return -1;
 	}
-	printf("%d\n",my_fd );
+	
 	struct sockaddr_un addr;
 	memset(&addr,0,sizeof(addr));
 	addr.sun_family = AF_UNIX;
 	strcpy(addr.sun_path, SOCK_PATH);
 		unlink(SOCK_PATH);
+		
+		
 	if((bind (my_fd, (struct sockaddr*)&addr, sizeof(struct sockaddr)))<0){
 		printf("Bind unsuccessful\n");
 		unlink(SOCK_PATH);
@@ -32,40 +35,6 @@ int main(){
 		return -1;
 	}
 
-	/*
-	//Prepare structs
-	memset(&hints,0,sizeof(hints));
-	hints.ai_family = AF_INET; //Only IPv4 for me AF_UNIX
-	//sockaddr_un
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags= AI_PASSIVE;
-	
-	if((aux2=getaddrinfo(NULL,MYPORT,&hints,&res)) !=0){
-		printf("getaddrinfo: \n");
-		return -1;
-	}
-
-	//Make it, bind it, listen
-	for(p= res; p!=NULL; p=p->ai_next){
-		if((my_fd=socket(p->ai_family, p->ai_socktype, p->ai_protocol))==-1){
-			perror("server: socket");
-			continue;
-		}
-
-		if((bind(my_fd,res->ai_addr,res->ai_addrlen))== -1){
-			close(my_fd);
-			perror("server:bind");
-			continue;
-		}
-		break;
-	}
-
-	if(p==NULL){
-		printf("server: failed to bind\n");
-		exit(1);
-	}
-	free(res);
-	*/
 
 	if((listen(my_fd, MAX_CALLS)) == -1){
 		perror("listen");
@@ -85,7 +54,8 @@ int main(){
 	}
 
 	//inet_ntop(AF_INET, &(their_addr.sin_addr),client,sizeof(client));
-	printf("Server: My man is online \n" );
+	client++;
+	printf("Server: My friend is %d online \n", client );
 
 	
 	//Talk to me
@@ -101,17 +71,17 @@ int main(){
 		if(aux.oper==0) //Copy
 		{
 
-			if(strcmp(data[aux.region],"")==0){
+			if((strcmp(data[aux.region],""))==0 ||(strcmp(data[aux.region],"\n"))==0  ){
 				//In case the region is empty
 			strcpy(aux.dados,"erro");
 			memcpy(msg,&aux,sizeof(Mensagem));
 
 			if((send(new_fd,msg, sizeof(aux),0))==-1){
 			perror("send"); //Do I need the number of bytes?
-			printf("My client disconnected\n");
+			printf("My client %d disconnected\n", client);
 			break;
 			}	
-			printf("My man %s tried to copy from region %d, but it's empty\n",client,aux.region);
+			printf("My friend %d tried to copy from region %d, but it's empty\n",client,aux.region);
 		}				
 			
 			
@@ -125,13 +95,13 @@ int main(){
 			printf("My client disconnected\n");
 			break;
 			}
-			printf("My man %s copied %s from region %d\n",client,data[aux.region],aux.region);
+			printf("My friend %d copied %s from region %d\n",client,data[aux.region],aux.region);
 		}
 			}
 		else if (aux.oper ==1 ) //Paste
 		{	
 			strcpy(data[aux.region],aux.dados);
-			printf("My client pasted %s to region %d\n",data[aux.region],aux.region);
+			printf("My friend %d pasted %s to region %d\n",client,data[aux.region],aux.region);
 		}
 		
 	}
